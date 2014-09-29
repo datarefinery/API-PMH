eb08d5e67a6a42906186aea713405e6e3f81a591
Shaun Osborne <shaun@cybergate9.net>
2014-09-29 09:26:17 +0100

    file/dir name changes

--- javascript/apipmhserver.js
+++ javascript/apipmhserver.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var logger = require('morgan');
-var routes = require('./routes/rethinkdb-apipmh.js'); 
+var routes = require('./rethinkdb-routes.js'); 
 
 var app = express();
 
