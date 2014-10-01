fcd38595e32daa4c1ff52013ae50f88e2a8e353f
Shaun Osborne <shaun@cybergate9.net>
2014-10-01 18:54:57 +0100

    rethinkdb query fixes

---
+++ js-express-rethinkdb/apipmhserver.js
@@ -0,0 +1,22 @@
+// useful ref: http://coenraets.org/blog/2012/10/creating-a-rest-api-using-node-js-express-and-mongodb/
+
+var express = require('express');
+var logger = require('morgan');
+var routes = require('./rethinkdb-routes.js'); 
+
+var app = express();
+
+
+app.use(logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+
+app.use(routes.apiHeader);
+
+app.get('/id/objects/all/', routes.getAll);
+
+app.get('/id/objects/:id', routes.getRecord);
+
+app.get('/id/objects/*', routes.identifyAPI); 
+
+
+app.listen(3000);
+console.log('Listening on port 3000...');
